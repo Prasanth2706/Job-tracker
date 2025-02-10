48726e3e6cf4a2ff9495bee3faeff818cc207eb3
Prasanth S <101855216+Prasanth2706@users.noreply.github.com>
2025-02-10 13:33:50 +0530

    Updated nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,14 +42,14 @@ function Navbar({ jobs }) {
         <h1 className="text-xl font-semibold text-gray-800">Job Tracker</h1>
 
         {/* Center - Search Bar */}
-        <div className="flex items-center bg-gray-100 px-3 py-2 rounded-md w-64">
+        {/* <div className="flex items-center bg-gray-100 px-3 py-2 rounded-md w-64">
           <Search className="w-5 h-5 text-gray-500" />
           <input
             type="text"
             placeholder="Search jobs..."
             className="bg-transparent outline-none px-2 text-gray-700 w-full"
           />
-        </div>
+        </div> */}
 
         {/* Right - Notifications & Profile */}
         <div className="flex items-center gap-4 relative">
